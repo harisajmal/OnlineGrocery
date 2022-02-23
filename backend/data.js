3c2aefe9b895efaf4e8a9a26d485113ab9b176f7
harisajmal <harisajmal29@gmail.com>
2022-02-23 20:52:55 +0000

    fetching data from backend

---
+++ backend/data.js
@@ -0,0 +1,54 @@
+export default {
+    products: [
+        {
+            _id: '001',
+        name: 'Bread',
+        category: 'bakery',
+        Image: '/Photos/products/bread.jpg',
+        price: 1.00,
+        brand: 'Co-Op',
+        rating: 4.00,
+        NumReviews: 5
+    },
+    {
+        _id: '002',
+        name: 'Coca-cola',
+        category: 'softdrinks',
+        Image: 'Photos/products/coca-cola.jpg',
+        price: 1.29,
+        brand: 'coke',
+        rating: 3.9,
+        NumReviews: 8
+    },
+    {
+        _id: '003',
+        name: 'Chicken',
+        category: 'Meat',
+        Image: '/Photos/products/bread.jpg',
+        price: 3.00,
+        brand: 'Co-Op',
+        rating: 4.7,
+        NumReviews: 9
+    },
+    {
+        _id: '004',
+        name: 'DairyMilk',
+        category: 'chocolate',
+        Image: '/Photos/products/bread.jpg',
+        price: 1.00,
+        brand: 'DairyMilk',
+        rating: 4.0,
+        NumReviews: 8
+    },
+    {
+        _id: '005',
+        name: 'Milk',
+        category: 'chilled',
+        Image: '/Photos/products/bread.jpg',
+        price: 1.49,
+        brand: 'Grahams',
+        rating: 4.3,
+        NumReviews: 4
+    }
+]
+}
